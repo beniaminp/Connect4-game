e6d426dd4003facd66a2ecf638c955242b87d76c
Dragos Pantiru <dragos.pantiru@asf.ro>
2016-08-26 13:59:38 +0300

    change tableBoard code

--- src/main/java/com/connect4/service/TableBoard.java
+++ src/main/java/com/connect4/service/TableBoard.java
@@ -6,31 +6,70 @@ public class TableBoard {
 	public static Integer[][] myBoard = new Integer[7][7];
 
 	public boolean checkWin(Integer[][] board, int i, int j) {
+
+		return checkVBack(board, i, j) || checkDiagonalRBack(board, i, j) || checkDiagonalLForward(board, i, j)
+				|| checkDiagonalLBack(board, i, j) || checkDiagonalRForward(board, i, j) || checkHBack(board, i, j)
+				|| checkHForward(board, i, j) || checkVForward(board, i, j);
+	}
+
+	private boolean checkVBack(Integer[][] board, int i, int j) {
 		if ((i - 3 >= 0) && (board[i][j] == board[i - 1][j] && board[i][j] == board[i - 2][j]
 				&& board[i][j] == board[i - 3][j])) {
 			return true;
-		} else if ((i - 3 >= 0 && j+3 <= 6) && (board[i][j] == board[i - 1][j + 1] && board[i][j] == board[i - 2][j + 2]
+		}
+		return false;
+	}
+
+	private boolean checkDiagonalRBack(Integer[][] board, int i, int j) {
+		if ((i - 3 >= 0 && j + 3 <= 6) && (board[i][j] == board[i - 1][j + 1] && board[i][j] == board[i - 2][j + 2]
 				&& board[i][j] == board[i - 3][j + 3])) {
 			return true;
-		} else if ((i + 3 <= 6 && j-3 >= 0) && (board[i][j] == board[i + 1][j - 1] && board[i][j] == board[i + 2][j - 2]
+		}
+		return false;
+	}
+
+	private boolean checkDiagonalLForward(Integer[][] board, int i, int j) {
+		if ((i + 3 <= 6 && j - 3 >= 0) && (board[i][j] == board[i + 1][j - 1] && board[i][j] == board[i + 2][j - 2]
 				&& board[i][j] == board[i + 3][j - 3])) {
 			return true;
-		}else if ((i - 3 >= 0 && j+3 <= 6) && (board[i][j] == board[i - 1][j + 1] && board[i][j] == board[i - 2][j + 2]
-				&& board[i][j] == board[i - 3][j + 3])) {
-			return true;
-		}else if ((i - 3 >= 0 && j-3 >= 0) && (board[i][j] == board[i - 1][j - 1] && board[i][j] == board[i - 2][j - 2]
+		}
+		return false;
+	}
+
+	private boolean checkDiagonalLBack(Integer[][] board, int i, int j) {
+		if ((i - 3 >= 0 && j - 3 >= 0) && (board[i][j] == board[i - 1][j - 1] && board[i][j] == board[i - 2][j - 2]
 				&& board[i][j] == board[i - 3][j - 3])) {
 			return true;
-		}else if ((i + 3 <= 6 && j+3 <= 6) && (board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2]
+		}
+		return false;
+	}
+
+	private boolean checkDiagonalRForward(Integer[][] board, int i, int j) {
+		if ((i + 3 <= 6 && j + 3 <= 6) && (board[i][j] == board[i + 1][j + 1] && board[i][j] == board[i + 2][j + 2]
 				&& board[i][j] == board[i + 3][j + 3])) {
 			return true;
-		}else if ((j - 3 >= 0) && (board[i][j] == board[i][j - 1] && board[i][j] == board[i][j - 2]
+		}
+		return false;
+	}
+
+	private boolean checkHBack(Integer[][] board, int i, int j) {
+		if ((j - 3 >= 0) && (board[i][j] == board[i][j - 1] && board[i][j] == board[i][j - 2]
 				&& board[i][j] == board[i][j - 3])) {
 			return true;
-		}else if ((j + 3 <= 6) && (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]
+		}
+		return false;
+	}
+
+	private boolean checkHForward(Integer[][] board, int i, int j) {
+		if ((j + 3 <= 6) && (board[i][j] == board[i][j + 1] && board[i][j] == board[i][j + 2]
 				&& board[i][j] == board[i][j + 3])) {
 			return true;
-		}else if ((i + 3 <= 6) && (board[i][j] == board[i + 1][j]) && (board[i][j] == board[i + 2][j])
+		}
+		return false;
+	}
+
+	private boolean checkVForward(Integer[][] board, int i, int j) {
+		if ((i + 3 <= 6) && (board[i][j] == board[i + 1][j]) && (board[i][j] == board[i + 2][j])
 				&& (board[i][j] == board[i + 3][j])) {
 			return true;
 		}
